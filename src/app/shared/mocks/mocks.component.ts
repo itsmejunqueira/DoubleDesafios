9b78067a211af41cdf14fc1b985be70df20f53c6
Leticia C. Junqueira <67600443+itsmejunqueira@users.noreply.github.com>
2023-01-03 00:24:46 -0300

    limpeza de codigo & reorganizando

--- src/app/shared/mocks/mocks.component.ts
+++ src/app/shared/mocks/mocks.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mocks',
   templateUrl: './mocks.component.html',
-  styleUrls: ['./mocks.component.css']
+  styleUrls: ['./mocks.component.scss']
 })
 export class MocksComponent implements OnInit {
 
